042ff8f8e46d3eb8353214209440287f80e6b013
Ayushi2716 <ashipatel2716@gmail.com>
2026-02-20 12:17:03 +0530

    added

---
+++ src/test/java/Practice/PaginationTest.java
@@ -0,0 +1,41 @@
+package Practice;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PaginationTest {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,3000)");
+		String input="Portable Charger";
+		//List<WebElement> nameList = driver.findElements(By.xpath("//table[@id=\"productTable\"]/tbody/tr/td[2]"));
+		List<WebElement> pagenext =driver.findElements(By.xpath("//a[@href='#']"));
+		int count=pagenext.size();
+		for(int i=1; i<=count; i++) {
+			List<WebElement> rows = driver.findElements(By.xpath("//table[@id='productTable']/tbody/tr"));
+			for(WebElement row:rows)
+			{
+				if(input.equals(row)) {
+				String price = driver.findElement(By.xpath("//table[@id='productTable']/tbody/tr/td[3]")).getText();
+				System.out.println(price);
+
+				}
+	
+	}
+	}
+	}
+}
+
+	
+
+
